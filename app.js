e2489bc6c5acb4a48f8f7391711684a0f6d5f0f9
Maxime Larrieu <LARRIEU Maxime>
2021-02-02 18:04:52 +0100

    :zap: start engine

--- app.js
+++ app.js
@@ -1,9 +1,44 @@
 const express = require('express')
+const inquirer = require('inquirer')
 const app = express();
 const port = 3000
 
-app.get('/', (req, res) => res.send('INDEX'))
+const GameMode = require('./engine/gamemode');
+const trois_cent_un = require('./engine/gamemodes/301');
+const GameMode301 = require('./engine/gamemodes/301')
 
-app.listen(port, () => {
-    console.log(`Listening on http://localhost:${port}`)
-})
+const getNbPlayers = () => {
+    inquirer
+        .prompt([
+            {
+                name: "gamemodes",
+                type: "list",
+                message: "Choisissez votre mode de jeu :",
+                choices: ["301", "Tour du monde"]
+            },
+            {
+                name: "number_players",
+                type: "number",
+                message: "Combien de joueurs participent ?"
+            }
+        ])
+        .then((answer) => {
+            if(answer.gamemodes) {
+                console.log(`Jeu choisi : ${answer.gamemodes}`)
+            } if(!answer.number_players) {
+                console.log("Un nombre est attendu")
+            } else {
+                console.log(`Nombre de joueurs : ${answer.number_players}`)
+                GameMode.getNbPlayers(answer.number_players)
+                console.log(GameMode.nbPlayers)
+                const troiscentun = new trois_cent_un()
+                console.log("301:" + troiscentun.nbDarts)
+                console.log(trois_cent_un.getNbPlayers)
+            }
+        })
+        .catch(error => {
+            console.log(error)
+        })
+}
+
+getNbPlayers();
