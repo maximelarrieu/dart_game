fea28defc687c008cd7cfc41a8fecb76ddecc28f
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-18 16:36:55 +0100

    :sparkles: temporaly remove inquirer cli function

--- app.js
+++ app.js
@@ -19,6 +19,12 @@ const cache = new SequelizeSimpleCache({
 
 cache.clear()
 
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
 require("./routers/game")(app);
 require("./routers/player")(app);
 
@@ -27,45 +33,45 @@ app.use(bodyParser.urlencoded({extended: true}))
 
 app.listen(port, () => console.log(`Port sur écoute : ${port}`));
 
-const getNbPlayers = () => {
-    inquirer
-        .prompt([
-            {
-                name: "gamemodes",
-                type: "list",
-                message: "Choisissez votre mode de jeu :",
-                choices: ["301", "Tour du monde"]
-            },
-            {
-                name: "number_players",
-                type: "number",
-                message: "Combien de joueurs participent ?"
-            }
-        ])
-        .then((answer) => {
-            if(answer.gamemodes) {
-                console.log(`Jeu choisi : ${answer.gamemodes}`)
-            } if(!answer.number_players) {
-                console.log("Un nombre est attendu")
-            } else {
-                console.log(`Nombre de joueurs : ${answer.number_players}`)
-                if (answer.gamemodes == "301") {
-                    let troiscentun = new trois_cent_un()
-                    troiscentun.getNbPlayers(answer.number_players)
-                    console.log("301 Nombre flechettes récupérées via super : " + troiscentun.nbDarts)
-                    console.log("Les "+ troiscentun.nbPlayers + " joueurs possèdent un score de " + troiscentun.score + " et "+ troiscentun.nbDarts +" fléchettes chacun")
-                    console.log(troiscentun.startGame())
-                } else {
-                    console.log("NOPE")
-                }
-            }
-        })
-        .catch(error => {
-            console.log(error)
-        })
-}
+// const getNbPlayers = () => {
+//     inquirer
+//         .prompt([
+//             {
+//                 name: "gamemodes",
+//                 type: "list",
+//                 message: "Choisissez votre mode de jeu :",
+//                 choices: ["301", "Tour du monde"]
+//             },
+//             {
+//                 name: "number_players",
+//                 type: "number",
+//                 message: "Combien de joueurs participent ?"
+//             }
+//         ])
+//         .then((answer) => {
+//             if(answer.gamemodes) {
+//                 console.log(`Jeu choisi : ${answer.gamemodes}`)
+//             } if(!answer.number_players) {
+//                 console.log("Un nombre est attendu")
+//             } else {
+//                 console.log(`Nombre de joueurs : ${answer.number_players}`)
+//                 if (answer.gamemodes == "301") {
+//                     let troiscentun = new trois_cent_un()
+//                     troiscentun.getNbPlayers(answer.number_players)
+//                     console.log("301 Nombre flechettes récupérées via super : " + troiscentun.nbDarts)
+//                     console.log("Les "+ troiscentun.nbPlayers + " joueurs possèdent un score de " + troiscentun.score + " et "+ troiscentun.nbDarts +" fléchettes chacun")
+//                     console.log(troiscentun.startGame())
+//                 } else {
+//                     console.log("NOPE")
+//                 }
+//             }
+//         })
+//         .catch(error => {
+//             console.log(error)
+//         })
+// }
 
-getNbPlayers();
+// getNbPlayers();
 
 const db = require('./models')
 db.sequelize.sync({force:true})
