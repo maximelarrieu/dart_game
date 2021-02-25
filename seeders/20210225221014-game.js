bd287cc44961323f40268e7bc79512ae4ccffbbb
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-25 23:56:12 +0100

    :zap: save time with seeders

---
+++ seeders/20210225221014-game.js
@@ -0,0 +1,31 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+    return queryInterface.bulkInsert('Games', [{
+      name: 'The game',
+      mode: '301',
+      createdAt: new Date(),
+      updatedAt: new Date()
+    }]);
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+      return queryInterface.bulkDelete('Games', null, {});
+  }
+};
