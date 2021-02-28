439f02b411039c295d8b7fa38e9527b50c986d10
maximelarrieu <maxime.larrieu@ynov.com>
2021-02-28 15:39:38 +0100

    :sparkles: try to understand how to use shot function

--- engine/gamemodes/301.js
+++ engine/gamemodes/301.js
@@ -1,3 +1,4 @@
+const { request } = require('express')
 const { nbPlayers } = require('../gamemode')
 const GameMode = require('../gamemode')
 
@@ -24,32 +25,33 @@ class trois_cent_un extends GameMode {
 
     // function shot (chaque joueur y passe 3x)
     shot() {
-        // Chaque score de chaque fleche baisse les points
-        let points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25]
-        let multi = [1, 2, 3]
+        // // Chaque score de chaque fleche baisse les points
+        // let points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25]
+        // let multi = [1, 2, 3]
         
-        const shoot = points[Math.floor(Math.random() * points.length)]
-        const multipli = multi[Math.floor(Math.random() * multi.length)]
+        // const shoot = points[Math.floor(Math.random() * points.length)]
+        // const multipli = multi[Math.floor(Math.random() * multi.length)]
 
-        for(let player = 1; player <= nbPlayers; player++) {
-            console.log(`Joueur ${index} lance sa flèche`)
-            for(let arrow = 1; arrow <= nbDarts; arrow++) {
-                const result = shoot * multipli
+        // for(let player = 1; player <= nbPlayers; player++) {
+        //     console.log(`Joueur ${index} lance sa flèche`)
+        //     for(let arrow = 1; arrow <= nbDarts; arrow++) {
+                const result = request.body.shot
                 console.log(result)
-                player.score -= result
-            }
-            if(player.score == 0) {
-                this.endGame(player)
-            }
-        }
+                return result
+                // player.score -= result
+            // }
+        //     if(player.score == 0) {
+        //         this.endGame(player)
+        //     }
+        // }
         // si points = 0 -> function end
     }
     
 
     // fucntion end
-    endGame(winner) {
-        console.log(`${winner} a gagné !`)
-    }
+    // endGame(winner) {
+    //     console.log(`${winner} a gagné !`)
+    // }
     // ....
 }
 
