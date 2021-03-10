0ac5c0a8af661dd9578e89f6959a22fab5789282
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-10 21:21:10 +0100

    :sparkles: get model in my class and alone game works

--- engine/gamemodes/301.js
+++ engine/gamemodes/301.js
@@ -2,6 +2,11 @@ const { request } = require('express')
 const { nbPlayers } = require('../gamemode')
 const GameMode = require('../gamemode')
 const GamePlayer = require('../../models').GamePlayer
+const Player = require('../../models').Player
+// const { sequelize } = require('../../models');
+// const sequelize = require('sequelize')
+// let player = new GamePlayer
+
 
 class trois_cent_un extends GameMode {
     
@@ -17,9 +22,87 @@ class trois_cent_un extends GameMode {
         this.shot()
     }
    
+    test(id, scoret, shots) {
+
+    }
 
     // function shot (chaque joueur y passe 3x)
-    shot(sector) {
+    shot(id, sector, test) {
+        // Récupération de la liste des joueurs de la partie
+        GamePlayer.findAll({
+            where: {
+                gameId: id,
+                inGame: true
+            },
+            include: [
+                {
+                    model: Player
+                }
+            ],
+            raw: true,
+            nest: true
+        })
+            // On passe l'object reçu
+            .then(function (players) {
+                console.log(players)
+                // Récupération de l'id du GamePlayer reçu
+                let player_id = players.map((p) => p.id)
+                // Récupèration du score total du GamePlayer
+                let player_score = players.map((p) => p.score)
+                // Récupération du nombre de tirs restants au joueur
+                let player_shots = players.map((p) => p.remainingShots)
+
+                let player = players.map((p) => p.playerId)
+
+                test = player
+                console.log(test)
+                const result = test
+                console.log(result)
+                // Update du GamePlayer
+                GamePlayer.update({
+                    // Soustraction du score total au sector reçu par la route
+                    score: player_score - sector,
+                    // On décrémente son nombre de tir
+                    remainingShots: player_shots - 1,
+                },
+                {
+                    where: {
+                        id: player_id
+                    }
+                })
+
+
+                    // Alors
+                    .then(function () {
+                        // Si un joueur ayant 0 de score est trouvé. Le inGame passe à false
+                        GamePlayer.update({
+                            inGame: false
+                        },
+                        {
+                            where: {
+                                id: player_id,
+                                score: 0
+                            }
+                        })
+                        // player.update({
+                        //     score: score - scoret,
+                        // })
+                        // .then(function (after) {
+                            console.log("test-------------")
+                            console.log(player)
+                            
+                        // })
+                    })
+                    console.log("fin:" + result)
+                    return test
+                    
+            })
+            // console.log("end:" + result)
+            return test
+        // console.log(player)
+        // for (let i in player) {
+        //     console.log(player.gameId)
+        // }
         // // Chaque score de chaque fleche baisse les points
         // let points = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 25]
         // let multi = [1, 2, 3]
@@ -30,9 +113,7 @@ class trois_cent_un extends GameMode {
         // for(let player = 1; player <= nbPlayers; player++) {
         //     console.log(`Joueur ${index} lance sa flèche`)
         //     for(let arrow = 1; arrow <= nbDarts; arrow++) {
-                const result = sector
-                console.log(result)
-                return result
+
                 // player.score -= result
             // }
         //     if(player.score == 0) {
