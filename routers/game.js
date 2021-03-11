27e8006e668d0956ca9eb3c4069336c2a12da1a8
maximelarrieu <maxime.larrieu@ynov.com>
2021-03-11 01:13:17 +0100

    :art: little problem with display'

--- routers/game.js
+++ routers/game.js
@@ -67,14 +67,6 @@ module.exports = app => {
     // Page du détail d'une partie
     router.get("/games/:id", jsonParser, urlencodedParser, function(req, res) {
         const passedId = req.params.id
-            Game.update({
-                currentPlayerId: GamePlayer.playerId
-            },
-            {
-                    where: {
-                        id: GamePlayer.gameId
-                    }
-            }),
             Game.findAll({
                 where: { 
                     id: passedId
@@ -97,16 +89,57 @@ module.exports = app => {
                         gameId: passedId
                     },
                     required: false
-                }
+                },
             ],
+            raw: true,
+            nest: true
             })
             .then(function (gp) {
-                res.render('games/details.pug', {
-                    game: gp,
-                    id: passedId,
-                    darts: troiscentun.nbDarts,
-                    score: troiscentun.score
+                console.log(gp)
+                let current_player = gp.map((p) => p.currentPlayerId)
+                let players = gp.map((p) => p.GamePlayers)
+                let shots = players.map((g) => g.remainingShots)
+                console.log(current_player)
+                console.log("shots: " + shots)
+                console.log(players.remainingShots)
+                GamePlayer.findAll({
+                    where: {
+                        playerId: current_player
+                    }
                 })
+                .then((gp) => {
+                    console.log("GP RECUP")
+                    console.log(gp)
+                    let id = gp.map((i) => i.id)
+                    let gameid = gp.map((g) => g.gameId)
+                    let shots = gp.map((s) => s.remainingShots)
+                    // let test = shots.map((s) => s)
+                    // console.log(test)
+                    if (shots == 0) {
+                        console.log("FINIIII")
+                        let player = gp.map((p) => p)
+                        console.log(player)
+                        let player_id = player.map((p) => p.playerId)
+                        console.log(player_id)
+                        let rand = Math.floor(Math.random() * player_id.length - id)
+                        let new_player = player_id[rand]
+                        console.log("cu : " + new_player)
+                        // troiscentun.randomize(id, gameid).then(response => {
+                            Game.update({
+                                currentPlayerId: new_player
+                            }, {
+                                where: {id: gameid}
+                            })
+                        // })
+                    }
+                })
+                res.render('games/details.pug', {
+                        game: gp,
+                        id: passedId,
+                        darts: troiscentun.nbDarts,
+                        score: troiscentun.score,
+                        players: players
+                    })
             })
     });
 
@@ -172,19 +205,34 @@ module.exports = app => {
     router.post("/games/:id/players/", jsonParser, urlencodedParser, function (req, res) {
         for (let c in req.body.checked) {
             GamePlayer.create({
-                include: [{
-                    model: Player
-                }],
+                include: [
+                    {
+                        model: Player
+                    }
+            ],
                 gameId: req.params.id,
                 playerId: req.body.checked[c],
                 remainingShots: troiscentun.nbDarts,
                 score: troiscentun.score,
-                inGame: score=0?false:true
+                inGame: true
             })
 
             // .then(function(gp) {
                 .then(function() {
-                    res.redirect(303, `/games/${req.params.id}/players`)
+                    troiscentun.startGame(req.params.id).then(response => {
+                        Game.update({
+                            currentPlayerId: response
+                        },
+                        {
+                            where: {
+                                id: req.params.id
+                            }
+                        })
+                        .then(function() {
+                            res.redirect(303, `/games/${req.params.id}/players`)
+                        })
+                    })
+                    
                 })
                 // counter.then(function(result) {
                 //     console.log(result)
@@ -217,18 +265,18 @@ module.exports = app => {
 
     // Créer un tir de joueur en base de données avec relation sur la partie
     router.post("/games/:id/shots", jsonParser, urlencodedParser, function (req, res) {
-        let test = 1
-        let player = troiscentun.shot(req.params.id, req.body.shot, test)
-        console.log("route: " + player)
-        GameShot.create({
-            sector: req.body.shot,
-            gameId: req.params.id,
-            playerId: player
-        })
-        .then(function(gs) {
-            res.redirect(`/games/${gs.gameId}`)
-        })
+        // troiscentun.shot(req.params.id, req.body.shot, ).then(response => {
+            const id = req.params.id
+            Game.findByPk(id)
+            .then(function(gs) {
+                console.log(gs)
+                let player = gs.currentPlayerId
+                troiscentun.shot(req.params.id, req.body.shot, gs.currentPlayerId)
+                // troiscentun.randomize(gs.playerId)
+                res.redirect(`/games/${req.params.id}`)
+            })
     })
+        // troiscentun.shot(req.params.id, req.body.shot, test
 
     //ROUTE FACULTATIVE
     // router.delete("/games/:id/shots/previous", function () {
